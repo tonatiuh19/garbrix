761685be765b9b4d4bf54a61c0f99f8cb9bf8adf
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2024-07-30 22:11:34 -0600

    feat: calculator

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrl: './calculator.component.css',
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements OnInit {
+  public priceForDisplay: number = 0.0;
+  inputSlider = new FormControl();
+  radioControl = new FormControl();
+  public minPrice = 10;
+  public maxPrice = 1000;
 
+  public comitionLabel = '6.5% + 60¢';
+  public comitionCurrency = 'USD';
+  public taxTitle = 'TAX';
+  public taxValue = 0.0;
+  public comition = 0.0;
+  public total = 0.0;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.comition = this.calculateComition(this.maxPrice);
+    this.inputSlider.valueChanges.subscribe((values) => {
+      this.priceForDisplay = values;
+      this.comition = this.calculateComition(values);
+      this.total = values - this.comition;
+    });
+
+    this.radioControl.valueChanges.subscribe((values) => {
+      if (values === 'US') {
+        this.comitionLabel = '6.5% + 60¢';
+        this.comitionCurrency = 'USD';
+        this.taxTitle = 'TAX';
+      } else {
+        this.comitionLabel = '6.5% + $5.00';
+        this.comitionCurrency = 'MXN';
+        this.taxTitle = 'IVA';
+      }
+    });
+
+    this.inputSlider.setValue(this.maxPrice);
+    this.radioControl.setValue('US');
+  }
+
+  private calculateComition(price: number): number {
+    if (this.radioControl.value === 'US') {
+      this.taxValue = (price * 0.065 + 5) * 0.08;
+      return price * 0.065 + 0.6;
+    } else {
+      this.taxValue = (price * 0.065 + 5) * 0.16;
+      return price * 0.065 + 5 + this.taxValue;
+    }
+  }
 }
--- src/app/components/calculator/calculator.module.ts
+++ src/app/components/calculator/calculator.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculatorComponent } from './calculator.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CalculatorComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   exports: [CalculatorComponent],
 })
 export class CalculatorModule {}
